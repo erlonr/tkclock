eb98433e094bb3e26a77853a2c37dc745c463191
Erlon <erlonreis00@gmail.com>
2025-01-10 11:26:42 -0300

    print data from .tasks file

--- tkclock.c
+++ tkclock.c
@@ -3,6 +3,11 @@
 #include <unistd.h>
 #include <string.h>
 
+
+const char RESET_COLOR[] = "\e[0m";
+const char BOLD_RED[] = "\e[1;31m";
+const char BOLD_GREEN[] = "\e[1;32m";
+
 typedef struct {
     unsigned short check;
     unsigned int time;
@@ -23,12 +28,12 @@ void start_task(int time) {
 }
 
 FILE* open_file(char *mode) {
-    FILE *task_file;
-    if (!(task_file = fopen("./.tasks", mode))) {
-        fprintf(stderr, "error: Could not open .task file\n");
+    FILE *tk_file;
+    if (!(tk_file = fopen("./.tasks", mode))) {
+        fprintf(stderr, "%serror:%s Could not open .tasks file\n", BOLD_RED, RESET_COLOR);
         exit(EXIT_FAILURE);
     }
-    return task_file;
+    return tk_file;
 }
 
 void task_already_completed(int time) {
@@ -48,37 +53,50 @@ void task_not_found() {
 }
 
 int search_task(char *t) {
-    FILE *t_file = open_file("r");
+    FILE *tk_file = open_file("r");
     task tk;
-    while (fscanf(t_file, "%hi,%i,%79[^\n]\n", &tk.check, &tk.time, tk.description) == 3) {
+    while (fscanf(tk_file, "%hi,%i,%79[^\n]\n", &tk.check, &tk.time, tk.description) == 3) {
         if (strstr(tk.description, t)) {
             if(tk.check)
                 task_already_completed(tk.time);
             else
                 start_task(tk.time);
-            fclose(t_file);
+            fclose(tk_file);
             return 0;
         }
     }
     task_not_found();
-    fclose(t_file);
+    fclose(tk_file);
 }
 
-/*void print_tasks() {
+void print_task(char *check, int time, char *description, const char *color) {
+    printf("\t%s%s%s (%03im) %s\n", color, check, RESET_COLOR, time, description);
+}
+
+void print_list_tasks() {
     FILE *tk_file = open_file("r");
-    while(fscan
-}*/
+    task tk;
+    printf("tasks list:\n");
+    while(fscanf(tk_file, "%hi,%i,%79[^\n]\n", &tk.check, &tk.time, tk.description) == 3) {
+        if (tk.check)
+            print_task("[x]", tk.time, tk.description, BOLD_GREEN);
+        else
+            print_task("[ ]", tk.time, tk.description, BOLD_RED);
+    }
+}
 
 int main(int argc, char *argv[]) {
     switch(argc) {
         case 1:
-            //print_tasks();
+            print_list_tasks();
             break;
         case 2:
             search_task(argv[1]);
             break;
         default:
-            fprintf(stderr, "error: the command takes only one argument.\n\texample: ./tkclock \"task description\"\n");
+            fprintf(stderr, 
+                    "%serror:%s the command takes only one argument.\n\texample: ./tkclock \"task description\"\n", 
+                    BOLD_RED, RESET_COLOR);
             exit(EXIT_FAILURE);
     }
     return 0;
